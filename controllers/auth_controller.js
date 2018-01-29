03d4d1d8340ab86e2a17ea91c8a21b3dc2621877
joeman098 <Jlogue908@gmail.com>
2018-01-29 14:24:05 -0500

    fixed?

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -12,7 +12,7 @@ exports.signin = function(req, res) {
 }
 exports.dashboard = function(req, res) {
  
-    res.render('dashboard');
+    res.render('dashboard', {displayName: req.user.displayName });
  
 }
 exports.forgot = function (req, res) {

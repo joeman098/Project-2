95f9f1521d2d3f07ea32ca1d2b3be9ce1cdb14c8
John LaTona <johnlatona@Johns-MBP.fios-router.home>
2018-02-01 23:17:45 -0500

    dashboard chatz werk

--- routes/profile_routes.js
+++ routes/profile_routes.js
@@ -79,11 +79,87 @@ module.exports = function(app, db) {
       ]
     }).then(function (feed) {
       var feeddata = feed
-      //çconsole.log(feeddata);
+      //console.log(feeddata);
       userDB.findOne({ where: { id: id } }).then(function(data) {
         res.render("profile", {
           feed:feed,
           id:data.id,
+          displayName: data.displayName,
+          profileImage: data.image,
+          aboutuser: data.about,
+          chan:chan,
+          class1: class1,
+          class2: class2,
+          class3: class3,
+          class4: class4,
+          class5: class5
+        });
+      });
+    })
+  });
+
+  app.get("/dashboard/:chan?", function(req, res) {
+    var id = req.user.id;
+
+    var chan = req.params.chan;
+    if (!chan){
+      chan = "general"
+    }
+    var class1;
+    var class2;
+    var class3;
+    var class4;
+    var class5;
+    if(chan === "general") {
+      class1 = "nav-links highlighted";
+      class2 = "nav-links";
+      class3 = "nav-links";
+      class4 = "nav-links";
+      class5 = "nav-links";
+    }
+    else if(chan === "gaming_chat") {
+      class1 = "nav-links";
+      class2 = "nav-links highlighted";
+      class3 = "nav-links";
+      class4 = "nav-links";
+      class5 = "nav-links";
+    }
+    else if(chan === "tv_chat") {
+      class1 = "nav-links";
+      class2 = "nav-links";
+      class3 = "nav-links highlighted";
+      class4 = "nav-links";
+      class5 = "nav-links";
+    }
+    else if(chan === "movie_chat") {
+      class1 = "nav-links";
+      class2 = "nav-links";
+      class3 = "nav-links";
+      class4 = "nav-links highlighted";
+      class5 = "nav-links";
+    }
+    else if (chan === "super-bowl-xxx_giggity"){
+      class1 = "nav-links";
+      class2 = "nav-links";
+      class3 = "nav-links";
+      class4 = "nav-links";
+      class5 = "nav-links highlighted";
+    }
+
+    db.Feed.findAll({
+      where:{
+        channel: chan
+      },
+      order:[
+        ['id', 'DESC'],
+      ]
+    }).then(function (feed) {
+      var feeddata = feed
+      //console.log(feeddata);
+      userDB.findOne({ where: { id: id } }).then(function(data) {
+        res.render("dashboard", {
+          feed:feed,
+          id: id,
           displayName: data.displayName,
           profileImage: data.image,
           aboutuser: data.about,

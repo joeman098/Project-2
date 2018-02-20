57abf061dc269e4b161b1584c9810862bb607214
Bradley Pulaski <bradpulaski@gmail.com>
2018-02-20 14:42:38 -0500

    cleaned up

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -5,62 +5,7 @@ var db = require("../../models");
 
 module.exports = function (passport) {
   var User = db.User;
-  // var LocalStrategy = require("passport-local").Strategy;
 
-  // passport.use(
-  //   "local-signup",
-  //   new LocalStrategy(
-  //     {
-  //       usernameField: "email",
-
-  //       passwordField: "password",
-
-  //       passReqToCallback: true // allows us to pass back the entire request to the callback
-  //     },
-
-  //     function(req, email, password, done) {
-  //       var generateHash = function(password) {
-  //         return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
-  //       };
-
-  //       User.findOne({
-  //         where: {
-  //           email: email
-  //         }
-  //       }).then(function(user) {
-  //         if (user) {
-  //           return done(null, false, {
-  //             message: "That email is already taken"
-  //           });
-  //         } else {
-  //           var userPassword = generateHash(password);
-
-  //           var data = {
-  //             email: email,
-
-  //             password: userPassword,
-
-  //             displayName: req.body.displayName,
-
-  //             image: req.body.ProfileImage,
-
-  //             interests: req.body.interests
-  //           };
-
-  //           User.create(data).then(function(newUser, created) {
-  //             if (!newUser) {
-  //               return done(null, false);
-  //             }
-
-  //             if (newUser) {
-  //               return done(null, newUser);
-  //             }
-  //           });
-  //         }
-  //       });
-  //     }
-  //   )
-  // );
   //serialize
   passport.serializeUser(function (user, done) {
     done(null, user);
@@ -70,54 +15,6 @@ module.exports = function (passport) {
     done(null, user);
   });
 
-  //LOCAL SIGNIN
-  // passport.use(
-  //   "local-signin",
-  //   new LocalStrategy(
-  //     {
-  //       // by default, local strategy uses username and password, we will override with email
-
-  //       usernameField: "email",
-
-  //       passwordField: "password",
-
-  //       passReqToCallback: true // allows us to pass back the entire request to the callback
-  //     },
-
-  //     function(req, email, password, done) {
-  //       var User = user;
-
-  //       var isValidPassword = function(userpass, password) {
-  //         return bCrypt.compareSync(password, userpass);
-  //       };
-
-  //       User.findOne({
-  //         where: {
-  //           email: email
-  //         }
-  //       })
-  //         .then(function(user) {
-  //           if (!user) {
-  //             req.flash("error", "No account with that email address exists.");
-  //             return done(null, false, {});
-  //           }
-
-  //           if (!isValidPassword(user.password, password)) {
-  //             req.flash("error", "Incorrect password.");
-  //             return done(null, false, {});
-  //           }
-
-  //           var userinfo = user.get();
-  //           return done(null, userinfo);
-  //         })
-  //         .catch(function(err) {
-  //           console.log("Error:", err);
-  //           req.flash("error", "Something went wrong with your Signin");
-  //           return done(null, false, {});
-  //         });
-  //     }
-  //   )
-  // );
   passport.use(
     new twitchStrategy(
       {

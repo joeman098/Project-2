b6fbb8a6bc25b4be857805878ee32d7d0b322e8f
John LaTona <johnlatona@Johns-MacBook-Pro.local>
2018-02-20 13:04:35 -0500

    Dashboard render update

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -19,14 +19,30 @@ class Dashboard extends Component {
         });
     };
 
-    handleFormSubmit = event => {
+    handleSearchSubmit = event => {
         event.preventDefault();
     }
 
-}
+    render() {
+        <div id="profile-body">
+            <video autoPlay loop muted preload className="fullscreen-bg_video">
+                <source src="../../../video/Circuit_Background.mp4"></source>
+            </video>
+            <div id="profile-img-container">
+                <img src="" id="profile-img"/>
+                <h1>{/*messages.error*/}</h1>
+                <h1>{/*messages.info*/}</h1>
+            </div>
+            <div class="row" id="profile-page-header">
+                <h1 id="profile-header-background-type">s0cial3r</h1>
+            </div>
+            <Nav username={this.state.username} handleInputChange={this.handleInputChange} handleSearchSubmit={this.handleSearchSubmit}/>
 
-//render() {
-    
-//}
+            {/*Charle's shiz*/}
+            
+        </div>
+    }
+
+}
 
 export default Dashboard;

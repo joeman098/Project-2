d6d7dfb57684231ff5b037f9959892e6df462060
John LaTona <johnlatona@Johns-MacBook-Pro.local>
2018-02-21 12:56:15 -0500

    login title animation

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -7,15 +7,26 @@ import "./Login.css";
 class Login extends Component {
     state = {
         search: "",
-        transparency: "",
+        transparency: 0,
     };
 
     componentDidMount() {
         this.setState({
             search: "",
-            transparency: "",
+            transparency: 0,
         })
+        this.fadeInMessage();
     };
+
+    fadeInMessage = () => {
+        let timeoutId;
+        clearTimeout(timeoutId);
+        this.setState({transparency: this.state.transparency + 0.01});
+
+        if(this.state.transparency < 1) {
+            timeoutId = setTimeout(this.fadeInMessage, 10);
+        }
+    }
 
     handleInputChange = event => {
         const { name, value } = event.target;
@@ -30,7 +41,16 @@ class Login extends Component {
     }
 
     render() {
+        const messageStyle = {
+            color: `rgba(255, 255, 255, ${this.state.transparency}`
+        }
+
+        const oStyle = {
+            color: `rgba(100, 65, 165, ${this.state.transparency}`
+        }
+
         return (
+            
             <div id="main-page-container">
                 <video autoPlay loop muted preload="true" className="fullscreen-bg_video">
                     <source src="../../../video/Circuit_Background.mp4"></source>
@@ -42,8 +62,8 @@ class Login extends Component {
                     <div className="col s10 m10 l10 xl8" id="main-page-content">
                         <div className="col s12 m12 l12 xl10 offset-xl1" id="card-col">
                             <div id="login-card">
-                            <h1 className="welcomeMessage">Welcome to</h1>
-                            <h1 id="main-page-title">s<span id="o">0</span>cial3r</h1>
+                            <h1 className="welcomeMessage" style={messageStyle}>Welcome to</h1>
+                            <h1 id="main-page-title" style={messageStyle}>s<span id="o" style={oStyle}>0</span>cial3r</h1>
                                 <div className="card-content white-text" id="card-content">
                                     <div className="row" id="sign-in-row">
                                         <a href="http://localhost:3001/auth/twitch/callback"><SubmitButton className="btn" value="Login with Twitch" id="sign-in-button"/></a>

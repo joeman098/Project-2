f2c6120af58456bb3ea7ebea871ffdd88ee4ee16
John LaTona <johnlatona@Johns-MacBook-Pro.local>
2018-02-21 12:02:41 -0500

    reworked login page stuff

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
 import Input from "../../components/Form/Input";
+import LoginNav from "../../components/LoginNav";
 import SubmitButton from "../../components/Form/SubmitButton";
 import "./Login.css";
 
 class Login extends Component {
     state = {
-        email: "",
-        password: ""
+        search: "",
+        transparency: "",
     };
 
     componentDidMount() {
         this.setState({
-            email: "",
-            password: ""
+            search: "",
+            transparency: "",
         })
     };
 
@@ -23,18 +24,10 @@ class Login extends Component {
             [name]: value
         });
     };
-    //check with Joe on routing and authentication redirects?
 
-    // handleFormSubmit = event => {
-    //     event.preventDefault();
-    //     API.signin({
-    //         email: this.state.email,
-    //         password: this.state.password
-    //     })
-    //     .then(res => console.log(res))
-    //     .catch(err => console.log(err));
-    // }
-
+    handleSearchSubmit = event => {
+        event.preventDefault();
+    }
 
     render() {
         return (
@@ -43,36 +36,17 @@ class Login extends Component {
                     <source src="../../../video/Circuit_Background.mp4"></source>
                 </video>
                 <div className="row" id="main-page-content-row">
+                <LoginNav handleInputChange={this.handleInputChange} handleSearchSubmit={this.handleSearchSubmit}/>
                     <div className="col s1 m1 l1 xl2" id="left-bar-main">
                     </div>
                     <div className="col s10 m10 l10 xl8" id="main-page-content">
-                        <h1 id="main-page-title">s<span id="o">0</span>cial3r</h1>
                         <div className="col s12 m12 l12 xl10 offset-xl1" id="card-col">
-                            <div className="card" id="login-card">
+                            <div id="login-card">
+                            <h1 className="welcomeMessage">Welcome to</h1>
+                            <h1 id="main-page-title">s<span id="o">0</span>cial3r</h1>
                                 <div className="card-content white-text" id="card-content">
                                     <div className="row" id="sign-in-row">
-                                        <form id="signin" name="signin" method="post" action="signin">
-                                            <label htmlFor="email" id="login-email-label">Email Address</label>
-                                            <Input
-                                                className="text"
-                                                type="text"
-                                                id="email"
-                                                value={this.state.email}
-                                                onChange={this.handleInputChange}
-                                                name="email"
-                                            />
-                                            <label htmlFor="password" id="login-password-label">Password</label>
-                                            <Input
-                                                type="password"
-                                                id="password"
-                                                value={this.state.password}
-                                                onChange={this.handleInputChange}
-                                                name="password"
-                                            />
-
-                                            <SubmitButton className="btn" type="submit" value="Sign In" id="sign-in-button"/>
-                                        </form>
-                                        <a href="http://localhost:3001/auth/twitch/callback"><button>Login with Twitch</button></a>
+                                        <a href="http://localhost:3001/auth/twitch/callback"><SubmitButton className="btn" value="Login with Twitch" id="sign-in-button"/></a>
                                     </div>
                                 </div>
                             </div>

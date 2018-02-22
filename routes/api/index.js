4aa0bbc2b3e82d88a563e5289d46904dfba9fb70
joeman098 <Jlogue908@gmail.com>
2018-02-22 14:36:09 -0500

    mahybe

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const feedRoute = require("./feeds");
+const socialitRoute = require("./feeds");
 
 // Book routes
+router.use("/socialit",socialitRoute)
 router.use("/feeds", feedRoute);
 
 

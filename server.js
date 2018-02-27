1334c61f79bed082ce60ad66b7f56c9ba96cfa09
joeman098 <Jlogue908@gmail.com>
2018-02-27 15:00:53 -0500

    john does not know dawe

--- server.js
+++ server.js
@@ -146,7 +146,15 @@ process.on('unhandledRejection', function (reason, p) { // moar reasons for unha
 
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname+"./client/build"));
+// app.use(express.static(__dirname+"./client/build"));
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path')
+  console.log('YOU ARE IN THE PRODUCTION ENV')
+  app.use('/static', express.static(path.join(__dirname, '../build/static')));
+  app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, '../build/'))
+  });
+}
 
 
 // ==========For Passport=============
